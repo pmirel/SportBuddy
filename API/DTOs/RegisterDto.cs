7d7e019fccf61c9fa2eb69cb178cb11a4aaf3256
M <M@DESKTOP-J0D963F>
2021-07-20 16:06:20 +0300

    Handle errors

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,9 +4,10 @@ namespace API.DTOs
 {
   public class RegisterDto
   {
-      [Required]
+    [Required]
     public string Username { get; set; }
     [Required]
+    [StringLength(10, MinimumLength = 4)]
     public string Password { get; set; }
   }
 }
