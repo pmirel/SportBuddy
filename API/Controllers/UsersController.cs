f969851d526b7c2ebee34a69c3cc2ec4701bfe86
M <M@DESKTOP-J0D963F>
2021-07-21 17:29:25 +0300

    update user entity

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-
+  [Authorize]
   public class UsersController : BaseApiController
   {
     private readonly DataContext _context;
@@ -19,13 +19,13 @@ namespace API.Controllers
     }
 
     [HttpGet]
-    [AllowAnonymous]
+
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
       return await _context.Users.ToListAsync();
     }
     //api/users/3
-    [Authorize]
+
     [HttpGet("{id}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
